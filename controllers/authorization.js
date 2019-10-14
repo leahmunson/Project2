127fd441505344402ffd14cead0aa57fed62c9c0
Leah Munson <munsonleahr@gmail.com>
2019-10-14 12:44:23 -0700

    linking signup

--- controllers/authorization.js
+++ controllers/authorization.js
@@ -3,14 +3,15 @@ var router = express.Router();
 var db = require('../models');
 var bcrypt = require('bcrypt');
 
-//get route for secret clubhouse, if logged in will elt you in, otherwise will fail
-router.get('/secret',function(req,res){
-    if(req.session.user) {
-        res.render('securepage',req.session.user);
-    }else {
-        res.send('Please login.')
-    }
-})
+// //get route for secret clubhouse, if logged in will elt you in, otherwise will fail
+// router.get('/secret',function(req,res){
+//     if(req.session.user) {
+//         res.render('securepage',req.session.user);
+//     }else {
+//         res.send('Please login.')
+//     }nod
+// })
+
 //get route to retrieve all users info, only for dev, remove from production
 router.get('/allUsers',function(req,res){
     db.User.findAll().then(function(users){
