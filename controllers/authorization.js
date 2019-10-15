06f7b0c909792552f683c057c60532a3818c01de
Leah Munson <munsonleahr@gmail.com>
2019-10-15 13:14:25 -0700

    login page js code for form

--- controllers/authorization.js
+++ controllers/authorization.js
@@ -3,14 +3,14 @@ var router = express.Router();
 var db = require('../models');
 var bcrypt = require('bcrypt');
 
-//get route for secret clubhouse, if logged in will elt you in, otherwise will fail
-router.get('/secret',function(req,res){
-    if(req.session.user) {
-        res.render('securepage',req.session.user);
-    }else {
-        res.send('Please login.')
-    }nod
-})
+// //get route for secret clubhouse, if logged in will elt you in, otherwise will fail
+// router.get('/secret',function(req,res){
+//     if(req.session.user) {
+//         res.render('securepage',req.session.user);
+//     }else {
+//         res.send('Please login.')
+//     }nod
+// })
 
 // //get route to retrieve all users info, only for dev, remove from production
 // router.get('/allUsers',function(req,res){
@@ -29,7 +29,8 @@ router.post('/signup',function(req,res){
     console.log(req.body)
     db.User.create({
         name:req.body.name,
-        password:req.body.password
+        password:req.body.password,
+        district: req.body.district
     }).then(function(newUser){
         console.log(newUser)
         res.json(newUser);
