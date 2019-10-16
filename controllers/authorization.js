96d61fd9d2bae6c2dd2bcc719adc0486105b0f50
Leah Munson <munsonleahr@gmail.com>
2019-10-16 11:56:44 -0700

    editing auth file

--- controllers/authorization.js
+++ controllers/authorization.js
@@ -4,20 +4,13 @@ var db = require('../models');
 var bcrypt = require('bcrypt');
 
 //get route for logged in users homepage, if logged in will let you in, otherwise will fail
-router.get('/secret',function(req,res){
+router.get('TODO:/secret',function(req,res){
     if(req.session.user) {
         res.render('securepage',req.session.user);
     }else {
         res.send('Please login.')
     }nod
 })
-
-// //get route to retrieve all users info, only for dev, remove from production
-// router.get('/allUsers',function(req,res){
-//     db.User.findAll().then(function(users){
-//         res.json(users);
-//     })
-// })
 
 //loads signup form
 router.get('/signup',function(req,res){
